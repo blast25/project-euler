69f61c0e5b4f091e3d2d747510d96926d0aa4cea
blast25 <lukasceb@gmail.com>
2017-04-17 11:41:13 +0200

    14

---
+++ 14.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main () {
+    
+    long long start = 13,
+         end = 1000000,
+         bterm = 0,
+         terms,
+         copy,
+         result;
+     
+    while(start < end) {
+        
+        copy = start;
+        terms = 1;
+
+        while (copy != 1) {
+            copy = copy % 2 == 0 ? copy / 2 : 3*copy + 1;
+            terms++;
+            if (copy < 0)
+                printf("%lld", start);
+        }
+        if(bterm < terms) {
+            bterm = terms;
+            printf("%10lld%10lld\n", start, bterm);
+        }
+        start++;
+    }
+    //faster calculation time by using already known collatz sequences
+    return(0);
+}
+
